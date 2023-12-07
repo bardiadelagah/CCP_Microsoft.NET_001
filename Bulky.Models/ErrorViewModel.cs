d36057ee3688804d29055711a5f7c9b4ea4248d9
Your Name <bdelagah@gmail.com>
2023-12-08 02:06:54 +0330

    first commit

---
+++ Bulky.Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Bulky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Bulky.Utility/SD.cs
@@ -0,0 +1,15 @@
+// this class use for static details
+// this class use for all constance for the website
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Utility
+{
+    public static class SD 
+    {
+        
+    }
+}
